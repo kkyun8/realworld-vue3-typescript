4670447b2f443f3c82856fd6e9d8f337f2476dfa
kkyun8 <kim23302@icloud.com>
2022-07-01 09:49:14 +0900

    多国語対応

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,9 +3,9 @@
     <div class="container page">
       <div class="row">
         <div class="col-md-6 offset-md-3 col-xs-12">
-          <h1 class="text-xs-center">Sign in</h1>
+          <h1 class="text-xs-center">{{ t("signIn") }}</h1>
           <p class="text-xs-center">
-            <router-link to="/register">Need an account?</router-link>
+            <router-link to="/register">{{ t("needAnAccount") }}</router-link>
           </p>
           <ul v-if="hasError" class="error-messages">
             <li v-for="message in messages" :key="message">{{ message }}</li>
@@ -33,8 +33,10 @@ export default defineComponent({
 
     const hasError = computed(() => store.state.common.hasError);
     const messages = computed(() => store.state.common.messages);
+    const t = computed(() => store.state.common.t);
 
     return {
+      t,
       hasError,
       messages,
     };
